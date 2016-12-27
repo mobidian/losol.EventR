b9a490cd3cc52084d591d9f641fe4cddf899e784
Ole Kristian Losvik <losvik+github@gmail.com>
2016-12-27 05:05:19 +0100

    end of day

--- src/losol.EventR/Models/ApplicationUser.cs
+++ src/losol.EventR/Models/ApplicationUser.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 namespace losol.EventR.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
+
+    // TODO Add image tutorial https://social.technet.microsoft.com/wiki/contents/articles/34445.mvc-asp-net-identity-customizing-for-adding-profile-image.aspx
     public class ApplicationUser : IdentityUser
     {
         public string FullName { get; set; }
